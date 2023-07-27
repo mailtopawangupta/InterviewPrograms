5fc780d483d86677e38374494233e4f4c9d844ff
pawan.gupta <pawan.gupta@fiserv.com>
2023-07-27 11:34:59 +0530

    ReverseStringWithMinimulIteration

--- ReverseStringWithMinimulIteration.java
+++ ReverseStringWithMinimulIteration.java
@@ -1,7 +1,7 @@
-import java.lang.reflect.Array;
+import java.util.Arrays;
 
 public class ReverseStringWithMinimulIteration {
-    public static void main(String args[]){
+    public static void main(String[] args){
         String string = "pawangupta";
         char[] stringArray = string.toCharArray();
         char temp ;
